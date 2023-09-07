6de659b134067b1069362a1b99b5bb9a9c6a2056
158SHI <1580405516@qq.com>
2023-09-07 22:17:04 +0800

    9.7_AVLTree_second_commit

--- 9.6_AVL_Tree/AVL_tree.h
+++ 9.6_AVL_Tree/AVL_tree.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <utility>
+#include <assert.h>
 
 namespace shr
 {
@@ -15,11 +16,13 @@ namespace shr
 		AVLT_node* _left;
 		AVLT_node* _right;
 		AVLT_node* _parent;
+		int _bf; //balance factor
 
 		AVL_tree_node(std::pair<Key, Value> kv)
 			:_kv(kv), 
 			_left(nullptr), _right(nullptr), 
-			_parent(nullptr)
+			_parent(nullptr),
+			_bf(0)
 		{ }
 	};
 
@@ -29,15 +32,89 @@ namespace shr
 	private:
 		typedef AVL_tree_node<Key, Value> AVLT_node;
 
-
 	public:
 		AVL_tree()
 			:_root(nullptr)
 		{ }
 
-		bool insert(const pair<Key, Value>& kv)
+		bool insert(const std::pair<Key, Value>& kv)
 		{
+			AVLT_node* newNode = new AVLT_node(kv);
+			if (_root == nullptr)
+			{
+				_root = newNode;
+				return true;
+			}
 
+			Key key = kv.first;
+			AVLT_node* cur = _root;
+			AVLT_node* parent = nullptr;
+			while (cur)
+			{
+				if (key < cur->_kv.first)
+				{
+					parent = cur;
+					cur = cur->_left;
+				}
+				else if (key > cur->_kv.first)
+				{
+					parent = cur;
+					cur = cur->_right;
+				}
+				else {
+					return false;
+				}
+			}
+			if (key < parent->_kv.first) {
+				parent->_left = newNode;
+			}
+			else {
+				parent->_right = newNode;
+			}
+			newNode->_parent = parent;
+			cur = newNode;
+
+			//更新和检查平衡因子
+			while (parent)
+			{
+				//更新平衡因子
+				if (cur == parent->_left) {
+					parent->_bf--;
+				}
+				else {
+					parent->_bf++;
+				}
+
+				//检查平衡因子
+				if (parent->_bf == 0) {
+					break;
+				}
+				else if (parent->_bf == 1 || parent->_bf == -1)
+				{
+					cur = parent;
+					parent = parent->_parent;
+				}
+				else if (parent->_bf == 2 || parent->_bf == -2) //需要做平衡处理
+				{
+					if (parent->_bf == 2 && cur->_bf == 1) {
+						RotateToLeft(parent); //左单旋
+					}
+					else if (parent->_bf == -2 && cur->_bf == -1) {
+						RotateToRight(parent); //右单旋
+					}
+					else if (parent->_bf == 2 && cur->_bf == -1) {
+						RotateRightLeft(parent); //左双旋
+					}
+					else { //(parent->_bf == -2 && cur->_bf == 1)
+						RotateLeftRight(parent); //右双旋
+					}
+					break; //旋转后退出调整和检查
+				}
+				else {
+					assert(false);
+				}
+			}
+			return true;
 		}
 
 		~AVL_tree()
@@ -55,6 +132,136 @@ namespace shr
 			_destroy(root->_right);
 			delete root;
 			root = nullptr;
+		}
+
+		void RotateToLeft(AVLT_node* parent)
+		{
+			AVLT_node* cur = parent->_right;
+			AVLT_node* curLeftNode = cur->_left;
+			AVLT_node* ppNode = parent->_parent;
+
+			parent->_right = curLeftNode;
+			cur->_left = parent;
+			cur->_bf = parent->_bf = 0;
+			//更新父节点
+			parent->_parent = cur;
+			if (curLeftNode) {
+				curLeftNode->_parent = parent;
+			}
+			//重新链到AVL树
+			if (ppNode == nullptr)
+			{
+				_root = cur;
+				cur->_parent = nullptr;
+			}
+			else
+			{
+				if (parent == ppNode->_left) {
+					ppNode->_left = cur;
+				}
+				else {
+					ppNode->_right = cur;
+				}
+				cur->_parent = ppNode;
+			}
+		}
+
+		void RotateToRight(AVLT_node* parent)
+		{
+			AVLT_node* cur = parent->_left;
+			AVLT_node* curRight = cur->_right;
+			AVLT_node* ppNode = parent->_parent;
+
+			parent->_left = curRight;
+			cur->_right = parent;
+			cur->_bf = parent->_bf = 0;
+
+			parent->_parent = cur;
+			if (curRight) {
+				curRight->_parent = parent;
+			}
+
+			if (ppNode == nullptr)
+			{
+				_root = cur;
+				cur->_parent = nullptr;
+			}
+			else
+			{
+				if (parent == ppNode->_left) {
+					ppNode->_left = cur;
+				}
+				else {
+					ppNode->_right = cur;
+				}
+				cur->_parent = ppNode;
+			}
+		}
+
+		void RotateRightLeft(AVLT_node* parent)
+		{
+			AVLT_node* cur = parent->_right;
+			AVLT_node* curLeft = cur->_left;
+			int curLeftBf = curLeft->_bf; //根据curLeftBf的旧bf值调整各个节点的bf值
+
+			//复用接口时，会将三个转折处的节点的bf一律修改为 0
+			//这是不完全正确的，需要分情况对三个关键节点的bf值进行修正
+			RotateToRight(parent->_right);
+			RotateToLeft(parent);
+
+			if (curLeftBf == 0)
+			{
+				parent->_bf = 0;
+				cur->_bf = 0;
+				curLeft = 0;
+			} //新节点插入到curLeft的左边
+			else if (curLeftBf == -1)
+			{
+				cur->_bf = 1;
+				parent->_bf = 0;
+				curLeft->_bf = 0;
+			} //新节点插入到curLeft的右边
+			else if (curLeftBf == 1)
+			{
+				parent->_bf = -1;
+				cur->_bf = 0;
+				curLeft->_bf = 0;
+			}
+			else {
+				assert(false);
+			}
+		}
+
+		void RotateLeftRight(AVLT_node* parent)
+		{
+			AVLT_node* cur = parent->_left;
+			AVLT_node* curRight = cur->_right;
+			int curRightBf = curRight->_bf;
+
+			RotateToLeft(parent->_left);
+			RotateToRight(parent);
+
+			if (curRightBf == 0)
+			{
+				cur->_bf = 0;
+				parent->_bf = 0;
+				curRight->_bf = 0;
+			}
+			else if (curRightBf == 1)
+			{
+				cur->_bf = -1;
+				parent->_bf = 0;
+				curRight->_bf = 0;
+			}
+			else if (curRightBf == -1)
+			{
+				cur->_bf = 0;
+				parent->_bf = 1;
+				curRight->_bf = 0;
+			}
+			else {
+				assert(false);
+			}
 		}
 
 	private:
--- 9.6_AVL_Tree/test.cpp
+++ 9.6_AVL_Tree/test.cpp
@@ -1,11 +1,57 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
 #include <iostream>
+#include <string>
+#include "AVL_tree.h"
 
 using namespace std;
 
+void test1()
+{
+	shr::AVL_tree<int, string> tree;
+	tree.insert(make_pair(9, "nine"));
+	tree.insert(make_pair(12, "twelve"));
+	tree.insert(make_pair(14, "fourteen"));
+	tree.insert(make_pair(5, "five"));
+	tree.insert(make_pair(10, "ten"));
+	tree.insert(make_pair(15, "fifteen"));
+}
+
+void test2()
+{
+	shr::AVL_tree<int, string> tree;
+	tree.insert(make_pair(14, "fourteen"));
+	tree.insert(make_pair(12, "twelve"));
+	tree.insert(make_pair(17, "seventeen"));
+	tree.insert(make_pair(9, "nine"));
+	tree.insert(make_pair(13, "thirteen"));
+	tree.insert(make_pair(5, "five"));
+}
+
+void test3()
+{
+	shr::AVL_tree<int, string> tree;
+	tree.insert(make_pair(90, "90"));
+	tree.insert(make_pair(12, "60"));
+	tree.insert(make_pair(10, "70"));
+}
+
+void test4()
+{
+	shr::AVL_tree<int, string> tree;
+	pair<int, string> kv_arr[9] = { make_pair(16, "16"), make_pair(3, "3"), make_pair(7, "7"),
+	make_pair(11, "11"), make_pair(9, "9"), make_pair(26, "26"), make_pair(18, "18"),
+	make_pair(14, "14"), make_pair(15, "15") };
+	for (auto& e : kv_arr) {
+		tree.insert(e);
+	}
+}
+
 int main()
 {
-
+	//test1();
+	//test2();
+	//test3();
+	test4();
 	return 0;
 }
